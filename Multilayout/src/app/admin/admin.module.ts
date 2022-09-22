e683c388fe1c3835f6ca4e7dc5548cb353ec7888
Arjunc331 <arjunc331@gmail.com>
2022-09-22 11:51:21 +0530

    practice

--- Multilayout/src/app/admin/admin.module.ts
+++ Multilayout/src/app/admin/admin.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { DepartmentComponent } from './department/department.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    AdminDashboardComponent,
     EmployeeComponent,
-    DepartmentComponent
+    DepartmentComponent,
+    AdminDashboardComponent
   ],
   imports: [
     CommonModule,
